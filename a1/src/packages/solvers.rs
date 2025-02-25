92c1c119a9d6da45f772687f98234e5fdef48fd5
Rishi Sarkar <rishisarkar273@gmail.com>
2025-02-25 10:09:37 -0500

    added some functionality to solvers

--- a1/src/packages/solvers.rs
+++ a1/src/packages/solvers.rs
@@ -1,8 +1,10 @@
-use crate::Packages;
 use crate::packages::Dependency;
+use crate::Packages;
+use rpkg::debversion::DebianVersionNum;
+use std::collections::VecDeque;
 
 impl Packages {
-    /// Computes a solution for the transitive dependencies of package_name; when there is a choice A | B | C, 
+    /// Computes a solution for the transitive dependencies of package_name; when there is a choice A | B | C,
     /// chooses the first option A. Returns a Vec<i32> of package numbers.
     ///
     /// Note: does not consider which packages are installed.
@@ -11,11 +13,41 @@ impl Packages {
             return vec![];
         }
 
-        let deps : &Vec<Dependency> = &*self.dependencies.get(self.get_package_num(package_name)).unwrap();
+        let deps: &Vec<Dependency> = &*self
+            .dependencies
+            .get(self.get_package_num(package_name))
+            .unwrap();
         let mut dependency_set = vec![];
+        let mut worklist = Vec::new();
 
-        // implement worklist
+        for dependency in deps {
+            match dependency.first() {
+                None => {
+                    continue;
+                }
+                Some(first_option) => {
+                    worklist.push(first_option);
+                }
+            }
+        }
 
+        while let Some(first_option) = worklist.pop() {
+            dependency_set.push(first_option.package_num);
+            if let Some(dependencies) = self.dependencies.get(&first_option.package_num) {
+                for dependency in dependencies {
+                    match dependency.first() {
+                        None => {
+                            continue;
+                        }
+                        Some(first_option) => {
+                            if !dependency_set.contains(&first_option.package_num) {
+                                worklist.push(first_option);
+                            }
+                        }
+                    }
+                }
+            }
+        }
         return dependency_set;
     }
 
@@ -27,10 +59,51 @@ impl Packages {
         if !self.package_exists(package_name) {
             return vec![];
         }
-        let mut dependencies_to_add : Vec<i32> = vec![];
+        let mut dependencies_to_add: Vec<i32> = vec![];
 
-        // implement more sophisticated worklist
+        let mut worklist: VecDeque<i32> = VecDeque::new();
 
+        worklist.push_back(*self.get_package_num(package_name));
+
+        while let Some(current_package_num) = worklist.pop_front() {
+            if dependencies_to_add.contains(&current_package_num) {
+                continue;
+            }
+
+            dependencies_to_add.push(current_package_num);
+
+            if let Some(dependencies) = self.dependencies.get(&current_package_num) {
+                for dependency in dependencies {
+                    if self.dep_is_satisfied(dependency).is_none() {
+                        let selected_package = self.select_dependency(dependency);
+                        if let Some(package_num) = selected_package {
+                            worklist.push_back(package_num);
+                        }
+                    }
+                }
+            }
+
+            if current_package_num == *self.get_package_num(package_name) {
+                dependencies_to_add.pop();
+            }
+        }
         return dependencies_to_add;
+    }
+
+    fn select_dependency(&self, dep: &Dependency) -> Option<i32> {
+        let mut best_package_num = None;
+        let mut best_version: Option<&DebianVersionNum> = None;
+
+        for alternative in dep {
+            let package_num = alternative.package_num;
+            let version = self.available_debvers.get(&package_num)?;
+
+            if best_version.is_none() || version > best_version.unwrap() {
+                best_package_num = Some(package_num);
+                best_version = Some(version);
+            }
+        }
+
+        return best_package_num;
     }
 }
